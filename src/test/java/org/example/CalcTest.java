acd89a73e870dabf4715e4138edd24e1cf897db1
gyx-h <gaoyunxuan_2006@example.com>
2026-09-22 09:30:25 +0800

    add subtract test,make test fail

--- src/test/java/org/example/CalcTest.java
+++ src/test/java/org/example/CalcTest.java
@@ -4,11 +4,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class CalcTest {
-  Calc c = new Calc();
+  @Test
+  void testAdd(){
+    Calc c = new Calc();
+    assertEquals(5, c.add(2,3));
+  }
+  
 
     @Test
-    void testAddition() {
-        assertEquals(4, c.add(2,2));
+    void testSubtract() {
+      Calc c = new Calc();
+        assertEquals(2, c.add(5,2));
     }
 
 }
